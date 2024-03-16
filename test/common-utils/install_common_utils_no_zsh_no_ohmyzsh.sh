8aa10c2befbedf7f53f6f8698b5e021b127dc373
Bart <bartventer@outlook.com>
2024-03-16 09:41:10 +0200

    test(common-utils): add Negative check function

--- test/common-utils/install_common_utils_no_zsh_no_ohmyzsh.sh
+++ test/common-utils/install_common_utils_no_zsh_no_ohmyzsh.sh
@@ -5,14 +5,26 @@ set -e
 # shellcheck disable=SC1091
 source dev-container-features-test-lib
 
+# Negative check function
+negative_check() {
+    local name="$1"
+    shift
+    if "$@"; then
+        echo "❌ $name check failed."
+        exit 1
+    else
+        echo "✅  Passed '$name'!"
+    fi
+}
+
 # Definition specific tests
 check "jq" jq  --version
 check "curl" curl  --version
 check "git" git  --version
-check "zsh" zsh --version || true
+negative_check "zsh not installed" command -v zsh
 check "ps" ps --version
-check "Oh My Zsh! theme" test -e "$HOME"/.oh-my-zsh/custom/themes/devcontainers.zsh-theme || true
-check "zsh theme symlink" test -e "$HOME"/.oh-my-zsh/custom/themes/codespaces.zsh-theme || true
+negative_check "Oh My Zsh! theme not installed" test -e "$HOME"/.oh-my-zsh/custom/themes/devcontainers.zsh-theme
+negative_check "zsh theme symlink not installed" test -e "$HOME"/.oh-my-zsh/custom/themes/codespaces.zsh-theme
 
 # Report result
 reportResults
